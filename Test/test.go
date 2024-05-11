14b1adead83d3f1600caad8734c5693696a370c6
ThajunNajaah <Shariq@900>
2024-05-11 20:05:39 +0530

    adding test function

---
+++ Test/test.go
@@ -0,0 +1,7 @@
+package Test
+
+import "fmt"
+
+func Test() {
+	fmt.Println("Welcome to test file!")
+}
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/TestDevOps/Test"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+
+	Test.Test()
+
+}
